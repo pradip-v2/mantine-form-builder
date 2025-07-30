b287dd56da5aa1af7650d9bb083d2057a375d502
pradipthinkitive <pradip.bankar@thinkitive.com>
2025-07-30 22:28:41 +0530

    parked release changes

--- src/components/form-builder/index.ts
+++ src/components/form-builder/index.ts
@@ -1,3 +1,7 @@
 import FormBuilder from "./FormBuilder";
+import { RenderForm } from "./render-form/RenderForm";
+import ViewFormResponse from "./form-responses/ViewFormResponse";
 
 export { FormBuilder };
+export { RenderForm };
+export { ViewFormResponse };
--- src/components/form-builder/render-form/ComponentRenderWrapper.tsx
+++ src/components/form-builder/render-form/ComponentRenderWrapper.tsx
@@ -1,4 +1,4 @@
-import type { InputField } from "@/api";
+import type { InputField } from "@/types";
 import { Flex, Grid, Input } from "@mantine/core";
 import type React from "react";
 
